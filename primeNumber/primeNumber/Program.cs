660f4902b70eedb5cf1f07947bcaed4f577b8cf5
Monomee <hoangthth12@gmail.com>
2024-11-23 13:14:32 +0700

    primeNumber

---
+++ primeNumber/primeNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primeNumber
+{
+    internal class Program
+    {
+        static bool isPrime(int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+            int count = 0;
+            for (int i = 1; i <= number; i++)
+            {
+                count += (number % i == 0) ? 1 : 0;
+            }
+            return count == 2;
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                if (isPrime(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
