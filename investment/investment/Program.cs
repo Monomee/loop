6cfc65e35dc8ad42331fe5023f3e5d5143419765
Monomee <hoangthth12@gmail.com>
2024-11-23 13:14:19 +0700

    investment

---
+++ investment/investment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace investment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = 1.0;
+            int month = 1;
+            double intersetRate = 1.0;
+            Console.WriteLine("Enter investment amount: ");
+            money = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number of months: ");
+            month = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter annual interest rate in percentage: ");
+            intersetRate = Double.Parse(Console.ReadLine());
+            double totalInterset = 0;
+            for (int i = 0; i < month; i++)
+            {
+                totalInterset += money * (intersetRate / 100) / 12;
+            }
+            Console.WriteLine("Total of interset: " + totalInterset);
+            Console.ReadLine();
+        }
+    }
+}
